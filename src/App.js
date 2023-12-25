4cd9a57c78a779d7c52f1537bdcdfb1b4fb9c8d8
sperlus <salimiha@gmail.com>
2023-12-25 14:31:03 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hi Nobar! Me love you! 11:03
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi there! 6:15 pm
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
